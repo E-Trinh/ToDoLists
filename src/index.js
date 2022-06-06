6795161bb2f2b0293cab38fc57ede1fcb8607e1d
E-Trinh <ethantrinh@csus.edu>
2022-06-06 14:03:07 -0700

    Added mainController

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import { projectManage } from "./projectManage.js";
 import { displayController } from "./displayController.js";
 
-displayController.pageSetup();
+//mainController module, handles interaction between the view and model
+
+const mainController = (function() {
+    const initialize = () => {
+        displayController.pageSetup();
+    }
+
+    initialize();
+})();
